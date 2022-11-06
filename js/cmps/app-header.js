fde790828a35854aca056454ea031fcc64060e86
Cedrus81 <erez.eitan.97@gmail.com>
2022-11-06 23:48:49 +0200

    fixed header icon

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -3,7 +3,7 @@ export default {
     <header class="full">
         <nav class="flex align-center">
         <h1>Señora-Libros</h1>
-        <img class="header-icon" src="../icons/book.png" alt="">
+        <img class="header-icon" src="./icons/book.png" alt="">
         </nav>
     </header>
     `
